1a3c3c5adc1623bbfbb46145972c499549116bd2
Sarah Delaney <tree.pruner@gmail.com>
2020-05-08 15:26:23 -0700

    news tokens to dataframe working!

--- Week4_5_grams.R
+++ Week4_5_grams.R
@@ -5,7 +5,7 @@ require(sqldf)
 require(data.table)
 
 (opt <- quanteda_options()) # threads = 2
-quanteda_options(verbose = TRUE)
+#quanteda_options(verbose = TRUE)
 quanteda_options(threads = 4)
 
 # Load "bad words" file from Google.
@@ -16,315 +16,127 @@ profanity <- tolower(profanity)
 lemma <- rep("profanity", length(profanity))
 
 
-###################################################### read in all file ######################################################
+###################################################### news ######################################################
 
 # read in raw news file
-news_path <- "./data/news.rds"
-news <- readRDS(news_path)
+newsPath <- "./data/news.rds"
+news <- readRDS(newsPath)
 newsLines <- length(news)
 newsLines # 1010242
 class(news) # character
-news <- iconv(news, from = "UTF-8", to = "ASCII", sub = "")
 
-# read in raw blogs file
-blogs_path <- "./data/blogs.rds"
-blogs <- readRDS(blogs_path)
-blogsLines <- length(blogs)
-blogsLines # 899288
-class(blogs) # character
-blogs <- iconv(blogs, from = "UTF-8", to = "ASCII", sub = "")
-
-# read in raw twitter file
-twitter_path <- "./data/twitter.rds"
-twitter <- readRDS(twitter_path)
-twitterLines <- length(twitter)
-twitterLines # 2360148
-class(twitter) # character
-twitter <- iconv(twitter, from = "UTF-8", to = "ASCII", sub = "")
-
-# make one big file
-all <- list(blogs = blogs, news = news, twitter = twitter)
-
-# all <- c(news, blogs, twitter)
-
-all <- length(all) 
-all # 3
-class(all) # list
-head(all)
-
-rm("news", "newsLines", "news_path", "blogs", "blogsLines", "blogs_path", "twitter", "twitterLines", "twitter_path" )
+# take a sample and close main file
+set.seed = 941
+sampleSize <- 0.001
+newsSample <- sample(news, size = round(newsLines * sampleSize), replace = FALSE)
+newsSample <- iconv(newsSample, from = "UTF-8", to = "ASCII", sub = "")
+saveRDS(newsSample, "./data/newsSample.rds")
+rm("news", "newsPath", "newsLines")
 
 # Convert to sentances
-
-system.time(all <- lapply(all,function(x){
+system.time(news_sen <- lapply(newsSample,function(x){
   system.time(theText <- corpus(x))
-  system.time(sentences <- corpus_reshape(theText,to="sentences",verbose=FALSE))
+  system.time(sentences <- corpus_reshape(theText, to = "sentences", verbose = FALSE))
   sentences # return to caller
 }))
 
-
+# user  system elapsed 
+# 898.95    4.34  908.25 
+saveRDS(news_sen, "./data/news_sen.rds")
+class(news_sen) # list
+rm(newsSample)
 
 # corpus and cleaning
-news_corpus <- corpus(news)
-length(news_corpus) # 101242
-head(news_corpus, 1) # "He wasn't home alone, apparently."
+length(news_sen) # 1010
+head(news_sen, 2)  # "Ball was travel editor of the New Orleans Times-Picayune fro..."
+                      # "abrettman@oregonian.com ; oregonlive.com/playbooks-profits; ..."
+news_senV <- sapply(news_sen, paste0, collapse="") # turn into a vector for imput to tokens
 
-# close raw file
-rm(news)
-
-# run 10 times (25k to 7 k at 10th run)  
-news_5_coll10 <- setDT( tokens(corpus_sample(news_corpus, size = 70000, replace = FALSE) 
-                            , remove_punct = TRUE 
-                            , remove_numbers = TRUE
-                            , remove_url = TRUE
-                            , remove_symbols = TRUE) %>% 
-        tokens_select( stopwords('english'), selection = 'remove') %>%
+# ngrams
+news_2gram <- data.frame(Tokens = as.character (
+        tokens(corpus(news_senV) 
+        , remove_punct = TRUE 
+        , remove_numbers = TRUE
+        , remove_url = TRUE
+        , remove_symbols = TRUE) %>% 
         tokens_tolower() %>%
         tokens_replace(profanity, lemma, valuetype="fixed") %>%
-        tokens_wordstem() %>% 
-        textstat_collocations(method = "lambda", size = 4) %>% 
-        arrange(-count, collocation))
-        
+        tokens_ngrams( n = 2)
+          )
+, stringsAsFactors = FALSE)
 
 
-head(news_5_coll10, 50 ); tail(news_5_coll1, 50)
+news_3gram <- tokens(corpus(news_senV) 
+                     , remove_punct = TRUE 
+                     , remove_numbers = TRUE
+                     , remove_url = TRUE
+                     , remove_symbols = TRUE) %>% 
+  tokens_tolower() %>%
+  tokens_replace(profanity, lemma, valuetype="fixed") %>%
+  tokens_ngrams( n = 3)
+
+news_4gram <- tokens(corpus(news_senV) 
+                     , remove_punct = TRUE 
+                     , remove_numbers = TRUE
+                     , remove_url = TRUE
+                     , remove_symbols = TRUE) %>% 
+  tokens_tolower() %>%
+  tokens_replace(profanity, lemma, valuetype="fixed") %>%
+  tokens_ngrams( n = 4)
+
+news_5gram <- tokens(corpus(news_senV) 
+                     , remove_punct = TRUE 
+                     , remove_numbers = TRUE
+                     , remove_url = TRUE
+                     , remove_symbols = TRUE) %>% 
+  tokens_tolower() %>%
+  tokens_replace(profanity, lemma, valuetype="fixed") %>%
+  tokens_ngrams( n = 5)
 
 
 # what did it output?
-class(news_5_coll3) # data table
-sapply(news_5_coll3, class) 
-# collocation        count count_nested       length       lambda  z 
-# "character"    "integer"    "integer"    "numeric"    "numeric" "numeric"
+head(news_2gram ); tail(news_2gram)
+class(news_tok) # tokens
+length(news_tok) #1010
 
-# com5ne all 10 news samples
-news_5_coll <- sqldf("
-select collocation as phrase, sum(count) as cnt
-from (  select collocation, count
-        from news_5_coll1
-  union all                   
-        select collocation, count 
-        from news_5_coll2
-  union all                   
-        select collocation, count 
-        from news_5_coll3        
-  union all                   
-        select collocation, count 
-        from news_5_coll4
-  union all                   
-        select collocation, count 
-        from news_5_coll5
-  union all                   
-        select collocation, count 
-        from news_5_coll6
-  union all                   
-        select collocation, count 
-        from news_5_coll7
-  union all                   
-        select collocation, count 
-        from news_5_coll8
-  union all                   
-        select collocation, count 
-        from news_5_coll9
-  union all                   
-        select collocation, count 
-        from news_5_coll10        
-     )
-group by collocation                  
-order by collocation
-")
-nrow(news_5_coll) #24068
+news_DF<- as.data.table(sapply(news_tok, paste0, collapse=""))
+news_DF
+
+rm(news_DF, news_corpus, news_V)
 
 # query
 sqldf("select phrase, cnt 
-      from news_5_coll 
+      from news
       where phrase like 'presid%' ")
 
 
 # close news coprus file and sample files
-rm(news_corpus, news_5_coll1, news_5_coll2, news_5_coll3, news_5_coll4, news_5_coll5, news_5_coll6, news_5_coll7, news_5_coll8, news_5_coll9, news_5_coll10)
 
 
 # save news collocation
 saveRDS(news_5_coll, "./data/news_5_coll.rds") 
 
 
-###################################################### news file ######################################################
+###################################################### blogs file ######################################################
+# read in raw blogs file
+blogsPath <- "./data/blogs.rds"
+blogs <- readRDS(blogsPath)
+blogsLines <- length(blogs)
+blogsLines # 899288
+class(blogs) # character
+blogs <- iconv(blogs, from = "UTF-8", to = "ASCII", sub = "")
 
-
-
-
-# corpus and cleaning
-blogs_corpus <- corpus(blogs)
-length(blogs_corpus) # 899288
-head(blogs_corpus, 1) # "In the years thereafter, most of the Oil fields and platform..."
-
-# close raw file
-rm(blogs)
-
-# run 10 times
-blogs_5_coll10 <- setDT( tokens(corpus_sample(blogs_corpus, size = 75000, replace = FALSE)
-                             , remove_punct = TRUE 
-                             , remove_numbers = TRUE
-                             , remove_url = TRUE
-                             , remove_symbols = TRUE        ) %>% 
-                              tokens_select( stopwords('english'), selection = 'remove') %>%
-                              tokens_tolower() %>%
-                              tokens_replace(profanity, lemma, valuetype="fixed") %>%
-                              tokens_wordstem() %>% 
-                              textstat_collocations(method = "lambda", size = 4) %>% 
-                              arrange(-count, collocation) )
-
-head(blogs_5_coll1, 5 ); tail(blogs_5_coll1, 5)
-
-
-# what did it output?
-class(blogs_5_coll3) # data table
-sapply(blogs_5_coll3, class) 
-# collocation        count count_nested       length       lambda  z 
-# "character"    "integer"    "integer"    "numeric"    "numeric" "numeric"
-
-# com5ne all 10 blogs samples
-blogs_5_coll <- sqldf("
-select collocation as phrase, sum(count) as cnt
-from (
-        select collocation, count
-        from blogs_5_coll1
-  union all                   
-        select collocation, count 
-        from blogs_5_coll2
-  union all                   
-        select collocation, count 
-        from blogs_5_coll3        
-  union all                   
-        select collocation, count 
-        from blogs_5_coll4
-  union all                   
-        select collocation, count 
-        from blogs_5_coll5
-  union all                   
-        select collocation, count 
-        from blogs_5_coll6
-  union all                   
-        select collocation, count 
-        from blogs_5_coll7
-  union all                   
-        select collocation, count 
-        from blogs_5_coll8
-  union all                   
-        select collocation, count 
-        from blogs_5_coll9
-  union all                   
-        select collocation, count 
-        from blogs_5_coll10        
-     )
-group by collocation                  
-order by collocation
-")
-nrow(blogs_5_coll) # 11674
-blogs_5_coll
-
-# query
-sqldf("select phrase, cnt 
-      from blogs_5_coll 
-      where phrase like 'presid%' ")
-
-
-# close blogs coprus file and sample files
-rm(blogs_corpus, blogs_5_coll1, blogs_5_coll2, blogs_5_coll3, blogs_5_coll4, blogs_5_coll5, blogs_5_coll6, blogs_5_coll7, blogs_5_coll8, blogs_5_coll9, blogs_5_coll10)
-
-
-# save blogs collocation
-saveRDS(blogs_5_coll, "./data/blogs_5_coll.rds") 
 
 ###################################################### twitter file ######################################################
+# read in raw twitter file
+twitterPath <- "./data/twitter.rds"
+twitter <- readRDS(twitterPpath)
+twitterLines <- length(twitter)
+twitterLines # 2360148
+class(twitter) # character
+twitter <- iconv(twitter, from = "UTF-8", to = "ASCII", sub = "")
 
 
-
-
-# corpus and cleaning
-twitter_corpus <- corpus(twitter)
-length(twitter_corpus) # 2360148
-head(twitter_corpus, 1) # "How are you? Btw thanks for the RT. You gonna be in DC anyti..."
-
-# close raw file
-rm(twitter)
-
-# run 10 times (200000)
-twitter_quad_coll10 <- setDT( tokens(corpus_sample(twitter_corpus, size = 150000, replace = FALSE)
-                                     , remove_punct = TRUE 
-                                     , remove_numbers = TRUE
-                                     , remove_url = TRUE
-                                     , remove_symbols = TRUE        ) %>% 
-                                tokens_select( stopwords('english'), selection = 'remove') %>%
-                                tokens_tolower() %>%
-                                tokens_replace(profanity, lemma, valuetype="fixed") %>%
-                                tokens_wordstem() %>% 
-                                textstat_collocations(method = "lambda", size = 5) %>% 
-                                arrange(-count, collocation) )
-
-
-
-head(twitter_5_coll1, 5 ); tail(twitter_5_coll1, 5)
-
-
-# what did it output?
-class(twitter_5_coll3) # data table
-sapply(twitter_5_coll3, class) 
-# collocation        count count_nested       length       lambda  z 
-# "character"    "integer"    "integer"    "numeric"    "numeric" "numeric"
-
-# com5ne all 10 twitter samples
-twitter_5_coll <- sqldf("
-select collocation as phrase, sum(count) as cnt
-from (
-        select collocation, count
-        from twitter_5_coll1
-  union all                   
-        select collocation, count 
-        from twitter_5_coll2
-  union all                   
-        select collocation, count 
-        from twitter_5_coll3        
-  union all                   
-        select collocation, count 
-        from twitter_5_coll4
-  union all                   
-        select collocation, count 
-        from twitter_5_coll5
-  union all                   
-        select collocation, count 
-        from twitter_5_coll6
-  union all                   
-        select collocation, count 
-        from twitter_5_coll7
-  union all                   
-        select collocation, count 
-        from twitter_5_coll8
-  union all                   
-        select collocation, count 
-        from twitter_5_coll9
-  union all                   
-        select collocation, count 
-        from twitter_5_coll10        
-     )
-group by collocation                  
-order by collocation
-")
-nrow(twitter_5_coll) #24452
-twitter_5_coll
-
-# query
-sqldf("select phrase, cnt 
-      from twitter_5_coll 
-      where phrase like 'presid%' ")
-
-
-# close twitter coprus file and sample files
-rm(twitter_corpus, twitter_5_coll1, twitter_5_coll2, twitter_5_coll3, twitter_5_coll4, twitter_5_coll5, twitter_5_coll6, twitter_5_coll7, twitter_5_coll8, twitter_5_coll9, twitter_5_coll10)
-
-
-# save twitter collocation
-saveRDS(twitter_5_coll, "./data/twitter_5_coll.rds") 
 
 
 ###################################################### combined ######################################################
