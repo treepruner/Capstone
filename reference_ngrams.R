6cc6ca34c7ffe6d84f70903951bbf1e6be94d2f2
Sarah Delaney <tree.pruner@gmail.com>
2020-05-20 13:29:06 -0700

    changed naming on freq_n2grams to freq_2grams

--- reference_ngrams.R
+++ reference_ngrams.R
@@ -88,26 +88,27 @@ sqldf(" select count(*) from LL_test_n2 where c_w12 >5") # 235,664
 sqldf(" select count(*) from LL_test_n2 where c_w12 >6") # 192,769
 sqldf(" select count(*) from LL_test_n2 where c_w12 >7") # 162,411
 sqldf(" select count(*) from LL_test_n2 where c_w12 >8") # 139,977
-sqldf(" select count(*) from LL_test_n2 where c_w12 >=9") # 122,566 keeps zucchini squash
+sqldf(" select count(*) from LL_test_n2 where c_w12 >=9") # 122,566 keeps "zucchini squash"
 
-
+LL_test_n2 <- readRDS("./data/LL_test_n2.rds")
 
 # code runs, but can't view table 
-freq_n2grams <- LL_test_n2 %>%
+freq_2grams <- LL_test_n2 %>%
   mutate(
     Chi_value = -2 * LL
     , pvalue = pchisq(LL, df = 1)
+    , bigram = paste0(word1, "_", word2)
     ) %>%
   filter(pvalue < 0.05 & c_w12 >= 9) %>%
-  select (word1, word2, c_w12, c_w1, c_w2) %>%
+  select (word1, word2, c_w12, c_w1, c_w2, bigram) %>%
   arrange (word1, desc(c_w12) )
 
 
     select(word1, word2) %>%
       unite(bigram, word1, word2, sep = " ")
 
-head(freq_n2grams)
-class(freq_n2grams)  # object pvalue not found
+head(freq_2grams)
+class(freq_2grams)  # object pvalue not found
 
 
 # word1      word2  c_w12  c_w1  c_w2
@@ -119,10 +120,12 @@ class(freq_n2grams)  # object pvalue not found
 # 5 added      people    11  3938 35174
 # 6 additional people    10  5142 35174
 
-saveRDS(freq_n2grams, "./data/freq_n2grams")
+saveRDS(freq_2grams, "./data/freq_2grams")
 
+freq_2grams <- readRDS("./data/freq_2grams")
+head(freq_2grams)
 
-rm(all_2grams, all_2gramsLines, all_2gramsPath, cnt_n2w1, cnt_n2w2, cnt_n2w12, N2, LL_test_n2, freq_n2grams)
+rm(all_2grams, all_2gramsLines, all_2gramsPath, cnt_n2w1, cnt_n2w2, cnt_n2w12, N2, LL_test_n2, freq_2grams)
 
 
 
@@ -141,28 +144,44 @@ all_3gramsLines <- nrow(all_3grams)
 all_3gramsLines # 4,635,555
 head(all_3grams)
 
-# source    word1      word2      word3
-# 1   news       st      louis      plant
-# 2   news     mass automotive production
-# 3   news    local     online      sites
-# 4   news   people  applauded      plans
-# 5   news campaign    finance    records
-# 6   news  finance    records   released
+
+
+freq_3grams <-
+  sqldf("select word1, word2, word3, word1 || '_' || word2 || '_' || word3 as trigram, count (*) as cnt 
+        from all_3grams
+        group by word1, word2, word3 
+        having count(*) >= 2")
+
+saveRDS(freq_3grams, "./data/freq_3grams")
+
+head(freq_3grams); tail(freq_3grams)
+nrow(freq_3grams) # >8 13592; > 4 35674;  > 2 94756 ; >= 2 254850
+
+#   word1          word2        word3                        trigram cnt
+# 1    aa       baseball      tuesday            aa_baseball_tuesday   4
+# 2    aa           city championship           aa_city_championship   3
+# 3    aa       football championship       aa_football_championship   2
+# 4    aa          girls   basketball            aa_girls_basketball   2
+# 5    aa intersectional championship aa_intersectional_championship   3
+# 6    aa intersectional        final        aa_intersectional_final   3
+
+
  
+########################################### does not work begin ###########################################
+### get an error like an out of memory message when using the all_grams file. Worked with sample file.
+### not sure my formulas make sense anyway!
 
 # log likelihood ratio test
 # n3 =  trigram, w1 = word1, w2 = word2, w3 = word3
 
 # count for trigrams
-cnt_n3w1 <- all_3grams %>%   count(word1) # 172,675
-cnt_n3w2 <- all_3grams %>%   count(word2) # 181,708
 cnt_n3w3 <- all_3grams %>%   count(word3) # 194,622
 cnt_n3w12 <- all_3grams %>%   count(word1, word2) # 2,691,782
 cnt_n3w123 <- all_3grams %>%   count(word1, word2, word3) # 3,934,255
 N3 <-nrow(all_3grams)  # 4,635,555
 
-head(cnt_n3w1)
-head(cnt_n3w2)
+rm(all_3grams)
+
 head(cnt_n3w3)
 head(cnt_n3w12 )
 head(cnt_n3w123)
@@ -176,6 +195,7 @@ head(cnt_n3w123)
 
 # treat first 2 words as single word and follow n2gram LL formula
 #  w12 = w123, w1 = w12, w2 = w3
+
 
 LL_test_n3 <- cnt_n3w123 %>%
   left_join(cnt_n3w12, by = "word1" , "word2") %>%  # 
@@ -224,96 +244,10 @@ head(u_n3grams)
 # 6 aa    baseball frieder      1 meetings      1     1 0.00000357     1     0     0         0      0
 
  
+########################################### does not work end ###########################################
 
 
-######################################################
-# Create n4grams
-# and remove numbers, punctuation, repeated letters, single letter words
-######################################################
 
-# tidyverse 
-# http://uc-r.github.io/creating-text-features
-# tidytextmining.com/ngrams
-
-n4gram <- 
-  data.table(allCln) %>%
-  unnest_tokens(quadgram, 'allCln', token = "ngrams", n = 4) %>%
-  separate(quadgram, c("word1", "word2", "word3", "word4"), sep = " ") %>%
-  filter (  !word1 %in% stop_words$word
-            , !str_detect(word1, pattern = "[[:digit:]]") # words with numbers
-            , !str_detect(word1, pattern = "[[:punct:]]") # punctuation
-            , !str_detect(word1, pattern = "(.)\\1{2,}")  # 3 or more repeated letters
-            , !str_detect(word1, pattern = "\\b(.)\\b")   # single letter words            
-            , !word2 %in% stop_words$word
-            , !str_detect(word2, pattern = "[[:digit:]]") # words with numbers
-            , !str_detect(word2, pattern = "[[:punct:]]") # punctuation
-            , !str_detect(word2, pattern = "(.)\\1{2,}")  # 3 or more repeated letters
-            , !str_detect(word2, pattern = "\\b(.)\\b")   # single letter words
-            , !word3 %in% stop_words$word
-            , !str_detect(word3, pattern = "[[:digit:]]") # words with numbers
-            , !str_detect(word3, pattern = "[[:punct:]]") # punctuation
-            , !str_detect(word3, pattern = "(.)\\1{2,}")  # 3 or more repeated letters
-            , !str_detect(word3, pattern = "\\b(.)\\b")   # single letter words
-            , !word4 %in% stop_words$word
-            , !str_detect(word4, pattern = "[[:digit:]]") # words with numbers
-            , !str_detect(word4, pattern = "[[:punct:]]") # punctuation
-            , !str_detect(word4, pattern = "(.)\\1{2,}")  # 3 or more repeated letters
-            , !str_detect(word4, pattern = "\\b(.)\\b")   # single letter words
-              ) 
-saveRDS(n4gram, "./data/n4gram.rds")
-
-# what do we have?
-head(n4gram)
-#       word1   word2  word3      word4
-# 1    current   april   june    quarter
-# 2       spur  robust    job     growth
-# 3   american    iron   chef television
-# 4 background tasters  offer   comments
-# 5       twin  cities  clean     cities
-# 6     cities   clean cities  coalition
-
-
-# log likelihood ratio test
-# n4 =  n4gram, w1 = word1, w2 = word2, w3 = word3, w4 = word4
-
-# count for n4grams
-cnt_n4w1 <- n4gram %>% count(word1) # 25871
-cnt_n4w2 <- n4gram %>% count(word2) # 26176
-cnt_n4w3 <- n4gram %>% count(word3) # 27180
-cnt_n4w4 <- n4gram %>% count(word4) # 28840
-cnt_n4w123 <- n4gram %>% count(word1, word2, word3) # 110237
-cnt_n4w1234 <- n4gram %>% count(word1, word2, word3, word4) #112133
-N4 <-nrow(n4gram)  # 113892
- 
-# error at first join
-# Error: `suffix` must be a character vector of length 2, not a character vector of length 1
-# Run `rlang::last_error()` to see where the error occurred.
-LL_test_n4 <- cnt_n4w1234 %>%
-  left_join(cnt_n4w123, by = "word1" , "word2", "word3") %>%  # 
-  left_join(cnt_n4w4, by = "word4") %>% 
-  rename(c_w1 = n.y, c_w2 = n, c_w1234 = n.x) %>%
-  mutate( #  w12 = w1234, w1 = w123, w3 = w4,
-    p  =  c_w4  / N4
-    , p1 =  c_w1234 / c_w123
-    , p2 = (c_w4  - c_w1234) / (N4 - c_w123)
-    , LL = log((pbinom(c_w1234, c_w123, p)  * pbinom(c_w4 - c_w1234, N4 - c_w123, p ))
-               / 
-                 (pbinom(c_w1234, c_w123, p1) * pbinom(c_w4 - c_w1234, N4 - c_w123, p)))
-    
-  )
-head(LL_test_n4)
-saveRDS(LL_test_n4, "./data/LL_test_n4.rds")
-
-u_n4grams <- LL_test %>%
-  mutate(
-    Chi_value = -2 * LL
-    , pvalue = pchisq(LL, df = 1)) %>%
-  filter(pvalue < 0.05) %>%
-  #  select(word1, word2) %>%
-  #    unite(bigram, word1, word2, sep = " ")
-  
-  head(u_n4grams)
-saveRDS(u_n4grams, "./data/u_n4grams")
 
 
 
